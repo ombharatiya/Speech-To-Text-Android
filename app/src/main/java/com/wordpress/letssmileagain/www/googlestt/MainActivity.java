b258a964448c9e68308fa2f03b55d945541f9545
ombharatiya <om.dkcarmel@gmail.com>
2017-11-09 02:33:59 +0530

    App ready to use

--- app/src/main/java/com/wordpress/letssmileagain/www/googlestt/MainActivity.java
+++ app/src/main/java/com/wordpress/letssmileagain/www/googlestt/MainActivity.java
@@ -1,13 +1,79 @@
 package com.wordpress.letssmileagain.www.googlestt;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+/***
+ * @Copyright OM Bharatiya
+ *
+ */
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.R.attr.start;
+import static com.wordpress.letssmileagain.www.googlestt.R.id.mic;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    TextView txtView;
+
+    ImageView mic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtView  = (TextView) findViewById(R.id.textView);
+        mic = (ImageView) findViewById(R.id.mic);
+
+        mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"Say Something...",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+                if(intent.resolveActivity(getPackageManager())!=null) {
+                    startActivityForResult(intent, 5);
+                }
+                else {
+                    Toast.makeText(v.getContext(),"Your Device Doesn't Support Speech Intent", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==5) {
+            if(resultCode==RESULT_OK && data!=null) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                txtView.setText(result.get(0));
+            }
+        }
     }
 }
